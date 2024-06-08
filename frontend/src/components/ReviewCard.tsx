1c3dbb21b3ff331b02c2582d4600767e50df1fa4
José Naves Moura Neto <josenaves@gmail.com>
2024-06-08 13:14:15 +0200

    Fix frontend rating

--- frontend/src/components/ReviewCard.tsx
+++ frontend/src/components/ReviewCard.tsx
@@ -21,7 +21,7 @@ const ReviewCard: FC<CardProps> = ({ review }) => {
             </p>
             <p
                 className={`mt-6 max-w-[30ch] text-sm opacity-75`}
-            >{`${rating}/5`}</p>
+            >{`${rating}/10`}</p>
         </div>
     );
 };
